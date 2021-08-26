e0d53baded71dc2c3282cbd8b4050d6dbcbb945a
ceejayv328 <ceejayv328@gmail.com>
2021-08-26 17:36:07 +0800

    Add visual state manager sample

--- ExpensesApp/Views/CategoriesPage.xaml.cs
+++ ExpensesApp/Views/CategoriesPage.xaml.cs
@@ -26,6 +26,24 @@ namespace ExpensesApp.Views
         {
             base.OnAppearing();
             _categoriesPageViewModel.GetExpensesPerCategory();
+
+            SizeChanged += Categories_SizeChanged;
+        }
+
+        private void Categories_SizeChanged(object sender, EventArgs e)
+        {
+            string visualState = Width > Height ? "Landscape" : "Portrait";
+            VisualStateManager.GoToState(titleLabel, visualState);
+        }
+
+        private void Button_Pressed(object sender, EventArgs e)
+        {
+            VisualStateManager.GoToState((Button)sender, "Focused");
+        }
+
+        private void Button_Released(object sender, EventArgs e)
+        {
+            VisualStateManager.GoToState((Button)sender, "Normal");
         }
     }
 }
